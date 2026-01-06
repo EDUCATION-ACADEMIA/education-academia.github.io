e9ed2c0a1983fd22099bc7ba6b20888d6588cbf1
CURRENT AFFAIRS <124424071+EDUCATION-ACADEMIA@users.noreply.github.com>
2026-01-06 18:47:26 +0530

    d

--- assets/script.js
+++ assets/script.js
@@ -12,7 +12,10 @@ function toggleAnswer(btn) {
 }
 
 function toggleDark() {
+  // Toggle the 'dark' class on the body element
   document.body.classList.toggle('dark');
+  
+  // Optionally change the button text based on dark mode status
   const darkButton = document.getElementById('dark-mode-toggle');
   if (document.body.classList.contains('dark')) {
     darkButton.textContent = 'Switch to Light Mode';
