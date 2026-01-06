ad7b67f46432cb36620d0ba9d55f007d039357ae
CURRENT AFFAIRS <124424071+EDUCATION-ACADEMIA@users.noreply.github.com>
2026-01-06 18:46:02 +0530

    fccc

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
 function toggleAnswer(btn) {
   const ans = btn.nextElementSibling;
-  
-  // Toggle visibility of the answer
   if (ans.classList.contains("hidden")) {
     ans.classList.remove("hidden");
     btn.textContent = "Hide Answer";
@@ -13,16 +11,12 @@ function toggleAnswer(btn) {
   }
 }
 
-// Dark Mode Toggle Function
 function toggleDark() {
-  // Toggle the dark mode class on body
   document.body.classList.toggle('dark');
-  
-  // Optionally toggle the button text if needed
   const darkButton = document.getElementById('dark-mode-toggle');
   if (document.body.classList.contains('dark')) {
-    darkButton.textContent = 'Switch to Light Mode'; // Change text to indicate dark mode is on
+    darkButton.textContent = 'Switch to Light Mode';
   } else {
-    darkButton.textContent = 'Switch to Dark Mode'; // Change text back to light mode option
+    darkButton.textContent = 'Switch to Dark Mode';
   }
 }
